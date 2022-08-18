9e36f483b1b6f71429dabaabc9fd513d24149357
Brice Venayre <brice.venayre@gmail.com>
2022-08-18 17:05:21 +0200

    refacto optimized

--- optimized.py
+++ optimized.py
@@ -1,7 +1,6 @@
 """Optimized algorithm maximizing benefits from stocks."""
-import time
 import csv
-import tracemalloc
+from textwrap import dedent
 
 DATA_PATH = "./data/sienna1.csv"
 WALLET = 500
@@ -15,23 +14,21 @@ class Stock:
 
         Args:
             name (str): Name of a stock
-            cost (float): Cost of a stock
+            price (float): Price of a stock
             percentage_benefit (float): Benefit in percentage after 2 years of investment
         """
         self.name = name
         self.price = price
         self.percentage_benefit = percentage_benefit
         self.profit = self.calculate_profit()
-        self.weigth = self.calculate_weigth()
-        self.return_value = self.profit - self.price
 
     def __str__(self) -> str:
-        """Represent an action.
+        """Represent a stock.
 
         Returns:
             str: Stock's name
         """
-        return f"{self.name} - Profit {self.profit} - Price {self.price}"
+        return f"{self.name} - Profit {round(self.profit, 2)} - Price {self.price}"
 
     def calculate_profit(self) -> int:
         """Calculate benefit after two years of investment.
@@ -41,17 +38,9 @@ class Stock:
         """
         return self.price + self.price * (self.percentage_benefit / 100)
 
-    def calculate_weigth(self) -> int:
-        """Calculate benefit after two years of investment.
-
-        Returns:
-            int: Money received
-        """
-        return self.price * (self.percentage_benefit / 100)
-
 
 class StocksCombination:
-    """Class representing a combination of stocks."""
+    """Class representing a combination from a list of stocks."""
 
     def __init__(self, stocks_list: list[Stock]):
         """Initialize combinations instances.
@@ -63,7 +52,6 @@ class StocksCombination:
             stocks_list, key=lambda stock: stock.percentage_benefit, reverse=True
         )
         self.combination = self.get_combination()
-        self.total_price = self.get_total_price(self.combination)
 
     @staticmethod
     def get_total_price(stocks_list: list[Stock]) -> int:
@@ -96,32 +84,34 @@ class StocksCombination:
         return profit
 
     def get_combination(self) -> list:
-        """Get all possible combinations of stocks.
-
-        Args:
-            number (int): Number of stocks in a combination
-            all_combinations (list, optional): List of combinations. Defaults to [].
+        """Get all possible combinations of stocks with a greedy algorithm.
 
         Returns:
-            list: List of all combinations
+            list: List of stocks
         """
         total_price = 0
-        first_combination = []
+        best_combination = []
         for stock in self.stocks:
             if total_price + stock.price <= WALLET and stock.price > 0:
-                first_combination.append(stock)
+                best_combination.append(stock)
                 total_price += stock.price
-
-        return first_combination
+        return best_combination
 
     def display_best_combination(self) -> None:
         """Display best combinations of stocks."""
+        print("Stocks to purchase :\n")
         for stock in self.combination:
             print(stock)
+        profit = round(self.get_total_profit(self.combination), 2)
+        price = round(self.get_total_price(self.combination), 2)
         print(
-            f"Total profit : {self.get_total_profit(self.combination)} euros\
-            \nTotal price : {self.total_price} euros\
-            \nTotal return : {self.get_total_profit(self.combination) - self.total_price} euros"
+            dedent(
+                f"""
+                Total profit : {profit} euros
+                Total cost : {price} euros
+                Total return : {round(profit - price, 2)} euros
+                """
+            )
         )
 
 
@@ -146,15 +136,9 @@ def get_data(path: str) -> list:
 
 def main():
     """Run main program."""
-    start_time = time.time()
-    tracemalloc.start()
-
     stocks_list = get_data(DATA_PATH)
     combination = StocksCombination(stocks_list)
     combination.display_best_combination()
-
-    print(tracemalloc.get_tracemalloc_memory())
-    print(f"Total time : {time.time() - start_time} s")
 
 
 if __name__ == "__main__":
